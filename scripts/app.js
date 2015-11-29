14fa645a028417773d747e0a8feee1262a89ac51
Richard Lee <rblee88@gmail.com>
2015-11-29 15:02:11 -0600

    Use map with JS library instead of static image

--- scripts/app.js
+++ scripts/app.js
@@ -24,3 +24,20 @@ var source2   = $("#trip-blurb-template").html();
 var template2 = Handlebars.compile(source2);
 var html = template2({trips: trips, totalTripLength: totalTripLength});
 $('#trip-blurbs').html(html)
+
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiYXJieWxlZSIsImEiOiJjaWdoZ2xjZWM4M2lmdjhtN3g2NzQxcncyIn0.Ru65y4i-50cxaM7_UA-D_w';
+var mapOptions = {
+  zoomControl: false,
+  tileLayer: {
+    continuousWorld: false,
+    noWrap: true
+  }
+}
+var map = L.mapbox.map('map', 'arbylee.oa107lhc', mapOptions).setView([25, 0], 2);
+
+map.dragging.disable();
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
+if (map.tap) map.tap.disable();
