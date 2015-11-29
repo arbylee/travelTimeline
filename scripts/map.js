9a72d084dfe7d80934e1c41727cbb70f5728dc72
Richard Lee <rblee88@gmail.com>
2015-11-29 17:12:50 -0600

    Add markers to trips

--- scripts/map.js
+++ scripts/map.js
@@ -1,3 +1,5 @@
+import trips from "./data.js";
+
 const MAP_OPTIONS = {
   zoomControl: false,
   tileLayer: {
@@ -9,6 +11,9 @@ const MAP_OPTIONS = {
 export default function(){
   L.mapbox.accessToken = 'pk.eyJ1IjoiYXJieWxlZSIsImEiOiJjaWdoZ2xjZWM4M2lmdjhtN3g2NzQxcncyIn0.Ru65y4i-50cxaM7_UA-D_w';
   let map = L.mapbox.map('map', 'arbylee.oa107lhc', MAP_OPTIONS).setView([25, 0], 2);
+  trips.forEach(function(trip, index){
+    trip.marker = L.marker(trip.coordinates).addTo(map);
+  });
 
   map.dragging.disable();
   map.touchZoom.disable();
